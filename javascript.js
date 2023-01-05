96c49448caf58026d13c62a4b032bd01a376b495
ZackGrid <leautonac@gmail.com>
2023-01-05 15:54:03 -0300

    Add media query for cellphones size

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const backgroundImage = document.querySelector(".back-img");
+const mql = window.matchMedia('(max-width: 600px)');
+
+function screenTest(e) {
+  if (e.matches) {
+    /* the viewport is 600 pixels wide or less */
+    backgroundImage.src="images/phone-CUT-jase-bloor-oCZHIa1D4EU-unsplash.jpg"
+  } else {
+    /* the viewport is more than 600 pixels wide */
+    backgroundImage.src="images/CUT-jase-bloor-oCZHIa1D4EU-unsplash.jpg"
+  }
+}
+
+mql.addEventListener('change', screenTest);
