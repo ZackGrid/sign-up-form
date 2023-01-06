2b3071b80885212476f2a740ac6914c866d15b04
ZackGrid <leautonac@gmail.com>
2023-01-06 12:51:00 -0300

    Add first time check screen width & change max width again

--- javascript.js
+++ javascript.js
@@ -1,17 +1,25 @@
 
 const backgroundImage = document.querySelector(".back-img");
-const mql = window.matchMedia('(max-width: 700px)');
+const mql = window.matchMedia('(max-width: 1000px)');
 
-//check width of page for use with phones
+//check width of page for adapting
 function screenTest(e) {
   if (e.matches) {
-    /* the viewport is 700 pixels wide or less */
+    /* the viewport is 1000 pixels wide or less */
     backgroundImage.src="images/phone-CUT-jase-bloor-oCZHIa1D4EU-unsplash.jpg"
   } else {
-    /* the viewport is more than 700 pixels wide */
+    /* the viewport is more than 1000 pixels wide */
     backgroundImage.src="images/CUT-jase-bloor-oCZHIa1D4EU-unsplash.jpg"
   }
 }
+
+function checkScreen() {
+    if (mql.matches) {
+        backgroundImage.src="images/phone-CUT-jase-bloor-oCZHIa1D4EU-unsplash.jpg"
+    }
+}
+
+checkScreen(); 
 
 mql.addEventListener('change', screenTest);
 
