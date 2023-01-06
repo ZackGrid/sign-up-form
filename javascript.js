7030a7f3cc633e557fdd6102bb193084363d05b0
ZackGrid <leautonac@gmail.com>
2023-01-06 12:00:18 -0300

    Add form validation for inputs

--- javascript.js
+++ javascript.js
@@ -1,14 +1,29 @@
+
 const backgroundImage = document.querySelector(".back-img");
 const mql = window.matchMedia('(max-width: 1000px)');
 
 function screenTest(e) {
   if (e.matches) {
-    /* the viewport is 600 pixels wide or less */
+    /* the viewport is 1000 pixels wide or less */
     backgroundImage.src="images/phone-CUT-jase-bloor-oCZHIa1D4EU-unsplash.jpg"
   } else {
-    /* the viewport is more than 600 pixels wide */
+    /* the viewport is more than 1000 pixels wide */
     backgroundImage.src="images/CUT-jase-bloor-oCZHIa1D4EU-unsplash.jpg"
   }
 }
 
 mql.addEventListener('change', screenTest);
+
+//verify if password match
+const check = function() {
+    if (document.getElementById('password').value ==
+        document.getElementById('confirm-pwd').value) {
+        document.getElementById('confirm-pwd').style.border = "1px solid green";
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'Yay password is matching';
+    } else {
+        document.getElementById('confirm-pwd').style.border = "1px solid red";
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'password not matching';
+    }
+  }
